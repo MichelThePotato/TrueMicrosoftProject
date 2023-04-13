437b099c5414ff4033ca2e57c97b10534757ea1d
khabibi1 <89889412+khabibi1@users.noreply.github.com>
2023-04-12 20:40:17 -0400

    changes

---
+++ Projetbibliotheque/Projetbibliotheque/Services/ISrcRequeteSami.cs
@@ -0,0 +1,14 @@
+﻿using LesBibiliotheque.Data.Models;
+
+namespace Projetbibliotheque.Data.Services
+{
+    public interface ISrcRequeteSami
+    {
+        List<Usager> TrierLesBibliothequeParNom(List<Usager> liste);
+
+
+        List<Usager> Recherche(string Nom, int biblioId);
+
+        List<Tuple<string, int>> Agrege();
+    }
+}
--- Projetbibliotheque/TestsBibliotheque.Tests/BibliothequesTests.cs
+++ Projetbibliotheque/TestsBibliotheque.Tests/BibliothequesTests.cs
@@ -1,6 +1,5 @@
-﻿using LesBibiliotheque.Data.Models;
-using LesBibiliotheque.Data.Services;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using BibiliothequeAdmin.Data.Services;
+using BibiliothequeProjet.Data.Models;
 
 namespace Bibliotheque.Tests
 {
@@ -8,25 +7,25 @@ namespace Bibliotheque.Tests
     public class BibliothequesTests
     {
         [TestMethod]
-        public void TestAdd1Bibliotheque()
+        public void TestAdd1BibliothequeAndDelete()
         {
             //Arrange
-            var baseName = "DeleteUnUsager";
+            var baseName = "DeleteAddBibliothequeAdd1";
             var connexionString = $"Server=(localdb)\\mssqllocaldb;Database={baseName};Trusted_Connection=true;";
-            var usager = new Usager { Nom = "" };
+            var biblio = new UneBibliotheque { Nom = "tom" };
 
             //Ajoute un usager
-            using (var usagers = new Usagers(connexionString, ensureCreated: true))
+            using (var biblios = new Bibliotheques(connexionString, ensureCreated: true))
             {
                 //Act
-                usagers.Add(usager);
+                biblios.Add(biblio);
 
             }
             //Delete un usager
-            using (var usagers = new Usagers(connexionString))
+            using (var biblios = new Bibliotheques(connexionString))
             {
-                //Ac
-                var actual = usagers.Delete(usager.Id);
+                //Act
+                var actual = biblios.Delete(biblio.Id);
 
                 //Assert
                 Assert.IsTrue(actual);
@@ -35,5 +34,91 @@ namespace Bibliotheque.Tests
         }
 
 
+        [TestMethod]
+        public void TestDelete1BiblioInEmptyDb()
+        {
+            //Arrange
+            var baseName = "DeletingBiblioEmptyDb";
+            var connexionString = $"Server=(localdb)\\mssqllocaldb;Database={baseName};Trusted_Connection=true;";
+            var biblio = new UneBibliotheque { Nom = "bibliotheque1" };
+
+            using (var biblios = new Bibliotheques(connexionString, ensureCreated: true))
+            {
+                //Act
+                var actual = biblios.Delete(biblio.Id);
+
+                //Assert
+                Assert.IsFalse(actual);
+
+            }
+        }
+
+        [TestMethod]
+        public void TestUpdate1Biblio()
+        {
+            //Arrange
+            var baseName = "UpdateBiblio";
+            var connexionString = $"Server=(localdb)\\mssqllocaldb;Database={baseName};Trusted_Connection=true;";
+            var biblio = new UneBibliotheque { Nom = "biblio" };
+
+
+            using (var biblios = new Bibliotheques(connexionString, ensureCreated: true))
+            {
+                //Act
+                biblios.Add(biblio);
+
+                var biblioModifie = biblios.Get(biblio.Id);
+                biblioModifie.Nom = "Modifié";
+                var actual = biblios.Update(biblioModifie);
+
+                Assert.IsTrue(biblio.Nom.Equals("Modifié"));
+
+
+            }
+        }
+        [TestMethod]
+        public void TestUpdate1BiblioFalse()
+        {
+            //Arrange
+            var baseName = "UpdateFalseBiblio";
+            var connexionString = $"Server=(localdb)\\mssqllocaldb;Database={baseName};Trusted_Connection=true;";
+            var biblio = new UneBibliotheque { Nom = "biblio" };
+
+            using (var biblios = new Bibliotheques(connexionString, ensureCreated: true))
+            {
+                //Act
+                biblios.Add(biblio);
+
+                var biblioModifie = new UneBibliotheque { Nom = "Modifié" };
+                var actual = biblios.Update(biblioModifie);
+
+                Assert.IsFalse(actual);
+
+
+            }
+        }
+        [TestMethod]
+        public void TestGetAll()
+        {
+            //Arrange
+            var baseName = "GetAll";
+            var connexionString = $"Server=(localdb)\\mssqllocaldb;Database={baseName};Trusted_Connection=true;";
+            var biblio1 = new UneBibliotheque { Nom = "biblio1" };
+            var biblio2 = new UneBibliotheque { Nom = "biblio2" };
+
+            using (var biblios = new Bibliotheques(connexionString, ensureCreated: true))
+            {
+                //Act
+                biblios.Add(biblio1);
+                biblios.Add(biblio2);
+
+                var actual = biblios.GetAll().Count();
+                var expected = 2;
+
+
+                Assert.AreEqual(expected, actual);
+
+            }
+        }
     }//class
 }//namespace
