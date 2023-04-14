c021f1e8841cf22d2eabda9198db558bf55de9d0
khabibi1 <89889412+khabibi1@users.noreply.github.com>
2023-04-13 23:10:56 -0400

    changes

--- Projetbibliotheque/Projetbibliotheque/Services/RequetesSami.cs
+++ Projetbibliotheque/Projetbibliotheque/Services/RequetesSami.cs
@@ -19,39 +19,37 @@ namespace Projetbibliotheque.Data.Services
 
         public List<Tuple<string, int>> Agregation()
         {
-            // Perform the LINQ query
             var result = (
-                from usager in dbContext.Usagers // Select from the Usagers DbSet
-                join bibliotheque in dbContext.Bibliotheques on usager.IdBibliotheque equals bibliotheque.Id // Perform a join with the Bibliotheques DbSet based on the IdBibliotheque property
-                orderby usager.Nom, bibliotheque.Nom descending // Sort by Nom property of Usagers and Nom property of Bibliotheques in descending order
+                from usager in dbContext.Usagers
+                join bibliotheque in dbContext.Bibliotheques on usager.IdBibliotheque equals bibliotheque.Id
+                orderby usager.Nom, bibliotheque.Nom descending
                 select new Tuple<string, int>(
-                    usager.Nom + " " + bibliotheque.Nom, // Project a calculated field by concatenating Nom property from Usagers and Nom property from Bibliotheques
-                    usager.Id + bibliotheque.Id) // Project another calculated field by adding Id property from Usagers and Id property from Bibliotheques
-                ).ToList(); // Convert the result to a List<Tuple<string, int>>
+                    usager.Nom + " " + bibliotheque.Nom,
+                    usager.Id + bibliotheque.Id)
+                ).ToList();
 
-            return result; // Return the result
+            return result;
         }
 
 
 
         public List<Usager> Recherche(string Nom, int biblioId)
         {
-            // Query the BibliothequeAdminDbContext to get Usagers and their related Bibliotheque entity
+
             var result = (
                 from usager in dbContext.Usagers
-                where usager.Nom.Contains(Nom) // Filter Usagers by name containing the provided Nom parameter
-                join bibliotheque in dbContext.Bibliotheques on usager.IdBibliotheque equals bibliotheque.Id // Perform a join with the Bibliotheques DbSet based on the IdBibliotheque property
-                where bibliotheque.Id == biblioId // Filter Bibliotheques by the provided biblioId parameter
+                where usager.Nom.Contains(Nom)
+                join bibliotheque in dbContext.Bibliotheques on usager.IdBibliotheque equals bibliotheque.Id
                 select new Usager
                 {
-                    // Set the properties of the Usager object
+
                     Id = usager.Id,
                     Nom = usager.Nom,
-                    // Set the properties of the related Bibliotheque entity
+
                     IdBibliotheque = bibliotheque.Id
                 }).ToList();
 
-            return result; // Return the list of Usagers with the detailed information about their related Bibliotheque entity
+            return result;
 
         }
         public List<Usager> TrierUsagerParNom()
